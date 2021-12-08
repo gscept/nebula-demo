2bd687678d8669068f376d7bc391851c4b062d3c
Fredrik Lindahl <fredrik-lindahl@outlook.com>
2021-12-08 15:09:02 +0100

    Rewrote movementmanager to use new update system

--- code/managers/movementmanager.cc
+++ code/managers/movementmanager.cc
@@ -13,6 +13,7 @@
 #include "util/random.h"
 #include "basegamefeature/managers/timemanager.h"
 #include "game/api.h"
+#include "basegamefeature/properties/transform.h"
 
 namespace Demo
 {
@@ -28,45 +29,17 @@ MovementManager::Create()
     n_assert(!MovementManager::HasInstance());
     Singleton = n_new(MovementManager);
 
-    Game::FilterCreateInfo filterInfo;
-    filterInfo.inclusive[0] = Game::GetPropertyId("WorldTransform");
-    filterInfo.access[0] = Game::AccessMode::WRITE;
-    filterInfo.inclusive[1] = Game::GetPropertyId("Movement");
-    filterInfo.access[1] = Game::AccessMode::WRITE;
-
-    filterInfo.numInclusive = 2;
-
-    Game::Filter filter = Game::CreateFilter(filterInfo);
-
-    Game::ProcessorCreateInfo processorInfo;
-    processorInfo.async = false;
-    processorInfo.filter = filter;
-    processorInfo.name = "MovementManager"_atm;
-    processorInfo.OnBeginFrame = [](Game::World*, Game::Dataset data)
+    Game::TimeSource const* const time = Game::TimeManager::GetTimeSource(TIMESOURCE_GAMEPLAY);
+    std::function UpdateFunc = [time](Game::WorldTransform& t, Movement& move) -> void
     {
-        Game::TimeSource const* const time = Game::TimeManager::GetTimeSource(TIMESOURCE_GAMEPLAY);
-        for (int v = 0; v < data.numViews; v++)
-        {
-            Game::Dataset::CategoryTableView const& view = data.views[v];
-            Math::mat4* const transforms = (Math::mat4*)view.buffers[0];
-            Movement* const moves = (Movement*)view.buffers[1];
-
-            for (IndexT i = 0; i < view.numInstances; ++i)
-            {
-                Math::mat4& t = transforms[i];
-                Movement& move = moves[i];
-
-                //Add a small random vector to the targets position.
-                float const x = move.wanderJitter * (Util::RandomFloatNTP() * move.wanderRadius);
-                float const z = move.wanderJitter * (Util::RandomFloatNTP() * move.wanderRadius);
-                Math::vec3 const wanderCircle = Math::vec3(x, 0, z);
-                move.direction = Math::normalize((move.direction + Math::normalize((wanderCircle + (Math::normalize(move.direction * move.wanderDistance))))));
-                t.position += (move.direction * move.speed * time->frameTime).vec;
-            }
-        }
+        //Add a small random vector to the targets position.
+        float const x = move.wanderJitter * (Util::RandomFloatNTP() * move.wanderRadius);
+        float const z = move.wanderJitter * (Util::RandomFloatNTP() * move.wanderRadius);
+        Math::vec3 const wanderCircle = Math::vec3(x, 0, z);
+        move.direction = Math::normalize((move.direction + Math::normalize((wanderCircle + (Math::normalize(move.direction * move.wanderDistance))))));
+        t.value.position += (move.direction * move.speed * time->frameTime).vec;
     };
-
-    Game::ProcessorHandle pHandle = Game::CreateProcessor(processorInfo);
+    Game::RegisterUpdateFunction(Game::GetWorld(WORLD_DEFAULT), "MovementManager.UpdateMovement"_atm, UpdateFunc);
 
     Game::ManagerAPI api;
     api.OnActivate = &MovementManager::OnActivate;
