731db6241915172143874482f701afbe1dc693e7
johannes <johannes.hirche@gmail.com>
2024-11-24 18:03:29 +0100

    - remove accidental comments

--- setup.bat
+++ setup.bat
@@ -20,10 +20,10 @@ call fips fetch
 echo Compiling external components
 call fips nebula set work %~dp0
 call fips nebula set toolkit %~dp0\..\nebula
-rem call fips physx build vc17 debug
-rem call fips anyfx setup
-rem call pip install py7zr
-rem call fips ultralight
+call fips physx build vc17 debug
+call fips anyfx setup
+call pip install py7zr
+call fips ultralight
 echo Generating solution
 call fips gen
 setlocal
